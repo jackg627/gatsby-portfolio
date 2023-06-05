ec7c6463905b6c72210d1d4bd67ca9d42d7fa4b0
Jack Glenn <jackg627@gmail.com>
2023-06-05 14:54:27 -0400

    Updated 'About Me'

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -8,11 +8,11 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    siteTitle: `Jack`,
+    siteTitleAlt: `Jack Glenn Portfolio`,
+    siteHeadline: `Jack Glenn`,
     siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteDescription: `Jack Glenn's software development portfolio site`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
     author: `@lekoarts_de`,
